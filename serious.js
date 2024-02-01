89b1a5a130e7dabd98e86e25e9378e83e9d12bfb
Lucifer-Phantom <144218257+Lucifer-Phantom@users.noreply.github.com>
2024-02-01 19:22:21 +0530

    Add files via upload

---
+++ serious.js
@@ -0,0 +1,23 @@
+
+
+(async ()=>{
+   await  new Promise((resolve,reject)=>{ setTimeout(()=>{let div=document.createElement('div')
+   div.innerHTML="initializing Hack Program...."
+   document.body.appendChild(div)
+   resolve()},2000)})
+   await  new Promise((resolve,reject)=>{ setTimeout(()=>{let div=document.createElement('div')
+   div.innerHTML="hacking your username...."
+   document.body.appendChild(div)
+   resolve()},2000)})
+   await  new Promise((resolve,reject)=>{ setTimeout(()=>{let div=document.createElement('div')
+   div.innerHTML="user name found...."
+   document.body.appendChild(div)
+   resolve()},2000)})
+   await  new Promise((resolve,reject)=>{ setTimeout(()=>{let div=document.createElement('div')
+   div.innerHTML="hacking all data"
+   document.body.appendChild(div)
+   resolve()},2000)})
+    
+})()
+
+
